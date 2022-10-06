d0e347f15d903e5c5033211015de5abb5f63a42f
otongcupu <68419591+otongcupu@users.noreply.github.com>
2022-10-06 16:03:11 +0700

    First upload

---
+++ fibonacci.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+
+func generate_fibonaccis() func() uint64 {
+	a, b := uint64(0), uint64(1)
+	return func() uint64 {
+		a, b = b, a+b
+		return a
+	}
+}
+
+
+func main() {
+    fib := generate_fibonaccis()
+	for j := 0; j < 10; j++ {
+	    fmt.Printf("%v ", fib())
+	}
+}
